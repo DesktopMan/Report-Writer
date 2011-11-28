8497fbc01a557e6a93b13688d7e782d54aa84fc9
Christian Auby <christian@auby.no>
2011-11-28 16:35:07 +0100

    Added initial project structure
    Folder structure and Visual Studio project in place.
    
    Initial GUI design and some thoughts around language specification
    added under Docs.

---
+++ ReportWriter/FrmMainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Report_Writer
+{
+    public partial class FrmMainWindow : Form
+    {
+        public FrmMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
